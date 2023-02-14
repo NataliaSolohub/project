d77c91e8468fe0498b2150823dc16e2aedadab59
Natasha <example@gmail.com>
2023-02-14 19:09:57 +0200

    first commit

---
+++ rich girl/script.js
@@ -0,0 +1,29 @@
+"use strict";
+// const category = 'toys';
+
+// console.log(`https://someurl.com/${category}/5`)
+
+// const user = 'Ivan';
+
+// alert(`Hello, ${user}`); // інтерполяція
+
+console.log('arr' + " - object");
+console.log(4 + "5");
+
+let incr = 10,
+    decr = 10;
+
+// ++incr;
+// --decr;
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(5%2);
+
+console.log(2 + 2 * 2 !== '6');
+
+const isChecked = false,
+    isClose = false;
+
+console.log(isChecked || !isClose);
